d266d686ab62a4b3dec00302e4904cac4e6d56a1
github.com <dhanukondasandhya8@gmail.com>
2023-12-14 23:28:30 +0530

    adding content

---
+++ varaiable3.sh
@@ -0,0 +1,8 @@
+varaiable3.sh       
+
+person1=ram
+person2=sai
+echo $person1: "Hi $person2"
+echo $person2: "Hello $person1"
+echo $person1: "good morning $person2"
+echo $person2: "Very good morning $person1"
