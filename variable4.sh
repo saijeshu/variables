998ee9a21b766ed3bc2c0dad9c8b7b0e9d13c6ed
github.com <dhanukondasandhya8@gmail.com>
2023-12-15 00:10:15 +0530

    passing arguments

---
+++ variable4.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+ echo "$PERSON1: Hi $PERSON2"
+ echo "$PERSON2: Hello $PERSON1"
+ echo "$PERSON1: good morning $PERSON2"
+ echo "$PERSON2: Very good morning $PERSON1"
