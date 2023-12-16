c4f484a5953742d751d09adae50c1e85ca57a98b
github.com <dhanukondasandhya8@gmail.com>
2023-12-16 15:07:59 +0530

    adding 2nd script

--- install-packages0.sh
+++ install-packages0.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-
 ID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/temp/$0-$TIMESTAMP.log"
+
+echo "script strated executing at $TIMESTAMP" &>> $LOGFILE
+
 
 VALIDATE(){
     if [ $? -ne 0 ]
@@ -14,7 +20,6 @@ VALIDATE(){
     else
        echo -e "$2...$G SUCCESS $N" 
     fi   
-
 }
 
 if [ $ID -ne 0 ]
@@ -29,12 +34,12 @@ echo "All arguments passed: $@"
 
 for package in $@
 do
-   yum list installed $package
+   yum list installed $package &>> $LOGFILE
    if [ $? -ne 0 ]
    then
-     yum install $package -y
+     yum install $package -y &>> $LOGFILE
      VALIDATE $? "Installation of package"
    else 
-      echo -e "$package is already installed ... $Y SKIPING $N"
+      echo -e "$package is already installed ... $Y SKIPPING $N"
    fi   
 done
